8fa15e1cede39a9a3aeb65091dd6b5b2aba0c440
Cristian <cristianfrancoraigosa@gmail.com>
2023-04-06 22:29:58 -0500

    Sign Up, avance

--- modelo.py
+++ modelo.py
@@ -1,29 +1,35 @@
 from datetime import *
 
+
 class User:
-    def __init__(self,username: str,name: str,password: str,birth :datetime ,mail :str):
+    def __init__(self, username: str, name: str, password: str, birth: datetime, mail: str):
         self.username = username
+        self.name = name
         self.password = password
         self.birth = birth
         self.datetime = datetime
         self.mail = mail
 
+
 class Administrator(User):
-    def __init__(self,username:str,name:str,password:str,birth:datetime,mail:str):
-        User.__init__(username,name,password,birth,mail)
+    def __init__(self, username: str, name: str, password: str, birth: datetime, mail: str):
+        User.__init__(username, name, password, birth, mail)
+
 
 class Program:
     def __init__(self):
         self.user_list = []
-    #REQUISITOS
-    def sign_up(self):
-        pass
+
+    # REQUISITOS
+    def sign_up(self, userName, name, password, birth, mail):
+        newUser = User(userName, name, password, birth, mail)
+        return newUser
+
     def log_in(self):
         pass
+
     def log_out(self):
         pass
+
     def delete_account(self):
         pass
-
-
-
