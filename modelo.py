a2e4b7ecc3feaeb624cd1812f402fb4f868f3111
Cristian <cristianfrancoraigosa@gmail.com>
2023-04-06 23:09:43 -0500

    Sign Up, avance 2

--- modelo.py
+++ modelo.py
@@ -1,4 +1,5 @@
 from datetime import *
+import re
 
 
 class User:
@@ -17,11 +18,18 @@ class Administrator(User):
 
 
 class Program:
+
     def __init__(self):
         self.user_list = []
 
     # REQUISITOS
     def sign_up(self, userName, name, password, birth, mail):
+        for user in self.user_list:
+            if user.username == userName:
+                return print("Error")
+        if not self.validar_correo(mail):
+            return print("Error")
+        birth = self.validar_fecha(birth)
         newUser = User(userName, name, password, birth, mail)
         return newUser
 
@@ -33,3 +41,20 @@ class Program:
 
     def delete_account(self):
         pass
+
+    def validar_correo(self,correo):
+        # Expresión regular para validar el formato del correo electrónico
+        patron = r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
+        # Verificar si el correo cumple con el patrón de la expresión regular
+        if re.match(patron, correo):
+            return True
+        else:
+            return False
+
+    def validar_fecha(self,fecha_string):
+        try:
+            fecha = datetime.strptime(fecha_string, '%Y-%m-%d %H:%M:%S')
+            return fecha
+        except ValueError:
+            return None
+
